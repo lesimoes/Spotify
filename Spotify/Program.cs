b73d8098c8b013998394482b88a5df5a6c127dfa
lesimoes <lesimoes0@gmail.com>
2020-03-03 23:22:41 -0300

    vai

---
+++ Spotify/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Spotify
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string url = "https://api.myjson.com/bins/14bgg6";
+            HttpWebRequest webrequest = (HttpWebRequest)WebRequest.Create(url);
+            webrequest.Method = "GET";
+            HttpWebResponse webresponse = (HttpWebResponse)webrequest.GetResponse();
+            Encoding enc = System.Text.Encoding.GetEncoding("utf-8");
+            System.IO.StreamReader responseStream = new StreamReader(webresponse.GetResponseStream(), enc);
+            string result = string.Empty;
+            result = responseStream.ReadToEnd();
+            webresponse.Close();
+            Console.WriteLine(result);
+            
+
+
+        }
+    }
+}
